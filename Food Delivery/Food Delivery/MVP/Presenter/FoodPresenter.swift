0e3fe6e0c2158355d4dd831bc0e33c468c3b6958
MarkDestroyer <markkir123@gmail.com>
2022-10-15 19:59:12 +0300

    update
    Added voice commands to scroll screen.

--- Food Delivery/Food Delivery/MVP/Presenter/FoodPresenter.swift
+++ Food Delivery/Food Delivery/MVP/Presenter/FoodPresenter.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Speech
 import UIKit
 
 protocol FoodPresentDelegate {
@@ -21,6 +22,15 @@ class FoodPresenter {
     var hamburgers = [Request]()
     var networkService = NetworkService()
     var delegate: Presenter?
+    var text = ""
+    var scrollView: UIScrollView?
+    var button: UIButton?
+    var isStart: Bool = false
+    
+    let audioEngine = AVAudioEngine()
+    let speechReconizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ru-RU"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var task : SFSpeechRecognitionTask!
     
     func GetFood() {
         networkService.GetFood { food in
@@ -38,7 +48,128 @@ class FoodPresenter {
         }
     }
     
+    func startSpeechRecognization(){
+        
+        self.button?.setImage(UIImage(systemName: "mic.fill"), for: .normal)
+        
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+            self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch let error {
+            print("Error comes here for starting the audio listner =\(error.localizedDescription)")
+        }
+        
+        guard let myRecognization = SFSpeechRecognizer() else {
+            print("Recognization is not allow on your local")
+            return
+        }
+        
+        if !myRecognization.isAvailable {
+            print("Recognization is free right now, Please try again after some time.")
+        }
+        
+        task = speechReconizer?.recognitionTask(with: request, resultHandler: { (response, error) in
+            guard let response = response else {
+                if error != nil {
+                    print(error.debugDescription)
+                } else {
+                    print("Problem in giving the response")
+                }
+                return
+            }
+            
+            let message = response.bestTranscription.formattedString
+            print("Message : \(message)")
+            self.text = message
+            
+            
+            var lastString: String = ""
+            for segment in response.bestTranscription.segments {
+                let indexTo = message.index(message.startIndex, offsetBy: segment.substringRange.location)
+                lastString = String(message[indexTo...])
+                self.text = String(message[indexTo...])
+            }
+            
+            switch lastString {
+                
+            case _ where lastString.contains("Вверх") || lastString.contains("вверх"):
+                DispatchQueue.main.async {
+                    UIView.animate(withDuration: 1, delay: 0, options: UIView.AnimationOptions.curveLinear, animations: {
+                        self.button?.setImage(UIImage(systemName: "arrow.up"), for: .normal)
+                        self.scrollView?.contentOffset.y = 0.0
+                    }, completion: nil)
+                }
+                
+            case _ where lastString.contains("Вниз") || lastString.contains("вниз"):
+                if self.scrollView?.contentOffset.y == 0.0 {
+                    DispatchQueue.main.async {
+                        UIView.animate(withDuration: 1, delay: 0, options: UIView.AnimationOptions.curveLinear, animations: {
+                            self.button?.setImage(UIImage(systemName: "arrow.down"), for: .normal)
+                            self.scrollView?.contentOffset.y = (self.scrollView?.contentOffset.y ?? 0) + 550
+                        }, completion: nil)
+                    }
+                }
+                
+            case _ where lastString.contains("Закуски") || lastString.contains("закуски"):
+                if self.scrollView?.contentOffset.y == 0.0 {
+                    DispatchQueue.main.async {
+                        UIView.animate(withDuration: 1, delay: 0, options: UIView.AnimationOptions.curveLinear, animations: {
+                            self.button?.setImage(UIImage(systemName: "mic.fill"), for: .normal)
+                            self.scrollView?.contentOffset.y = (self.scrollView?.contentOffset.y ?? 0) + 380
+                        }, completion: nil)
+                    }
+                }
+                
+            case _ where lastString.contains("Бургеры") || lastString.contains("бургеры"):
+                if self.scrollView?.contentOffset.y == 0.0 {
+                    DispatchQueue.main.async {
+                        UIView.animate(withDuration: 1, delay: 0, options: UIView.AnimationOptions.curveLinear, animations: {
+                            self.button?.setImage(UIImage(systemName: "mic.fill"), for: .normal)
+                            self.scrollView?.contentOffset.y = (self.scrollView?.contentOffset.y ?? 0) + 550
+                        }, completion: nil)
+                    }
+                }
+                
+            case _ where lastString.contains("Стоп") || lastString.contains("стоп"):
+                DispatchQueue.main.async {
+                    UIView.animate(withDuration: 1, delay: 0, options: UIView.AnimationOptions.curveLinear, animations: {
+                        self.button?.sendActions(for: .touchUpInside)
+                        self.scrollView?.contentOffset.y = (self.scrollView?.contentOffset.y ?? 0) - (self.scrollView?.contentOffset.y ?? 0)
+                    }, completion: nil)
+                }
+            default:
+                self.text = ""
+                
+            }
+            
+        })
+    }
+    
+    func cancelSpeechRecognization() {
+        self.button?.setImage(UIImage(systemName: "mic"), for: .normal)
+        audioEngine.stop()
+        task?.cancel()
+        request.endAudio()
+        audioEngine.inputNode.removeTap(onBus: 0)
+    }
+    
     func SetFoodDelegate(delegate: FoodPresentDelegate & UIViewController) {
         self.delegate = delegate
     }
+    
+    func SetScroll(scrollView: UIScrollView) {
+        self.scrollView = scrollView
+    }
+    
+    func SetButton(button: UIButton) {
+        self.button = button
+    }
+    
 }
