f30e2730c26b1fc1965cbbb21b294b96e299be54
MarkDestroyer <markkir123@gmail.com>
2022-10-16 22:05:32 +0300

    update
    Added TabBar.

---
+++ Food Delivery/Food Delivery/TabBar/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  Food Delivery
+//
+//  Created by Марк Киричко on 16.10.2022.
+//
+
+import UIKit
+
+class MenuController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
