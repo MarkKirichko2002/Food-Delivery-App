5b1b418574fc6202089015c5ce902939f755ebdf
MarkDestroyer <markkir123@gmail.com>
2022-10-20 00:11:49 +0300

    update
    Changed banner.

---
+++ Food Delivery/Food Delivery/Sticky Header/Cells/BannerCell.swift
@@ -0,0 +1,16 @@
+//
+//  BannerCell.swift
+//  Food Delivery
+//
+//  Created by Марк Киричко on 19.10.2022.
+//
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+    
+    static let identifier = "BannerCell"
+    
+    @IBOutlet weak var BannerImage: UIImageView!
+    
+}
